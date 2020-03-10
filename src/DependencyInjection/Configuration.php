8335452cf894f115381b68816e2156343724802b
Dominik Chrastecky <dominik@chrastecky.cz>
2020-03-10 10:58:09 +0100

    remove unused import

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,6 @@
 
 namespace Rikudou\BySquare\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
